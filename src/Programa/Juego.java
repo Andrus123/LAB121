b4ea82cc07b87e0efd8baca4972cba620ab4aa20
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-20 01:23:54 -0400

    Herencia: Ejercicio Programa

---
+++ src/Programa/Juego.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Programa;
+
+/**
+ *
+ * @author Andrés Aquin
+ */
+public class Juego extends Programa{
+    private String nombre;
+    private String categoria;
+    private String descripcion;
+    
+    public Juego(String emdv, int np, double v, double p,String n, String cat, String des){
+        super(emdv,np,v,p);
+        this.nombre = n;
+        this.categoria = cat;
+        this.descripcion = des;
+    }
+    public Juego(){
+        super();
+        this.nombre = "League of Legends";
+        this.categoria = "PC";
+        this.descripcion = "LOL es un MOPG de 5v5 en linea";
+    }
+    public void leer(){
+        super.leer();
+        System.out.println("Nombre del Juego: ");
+        setNombre(Leer.dato());
+        System.out.println("Categoria: ");
+        setCategoria(Leer.dato());
+        setNombre(Leer.dato());
+        System.out.println("Descripción: ");
+        setDescripcion(Leer.dato());
+    }
+    public void mostrar(){
+        super.mostrar();
+        System.out.println("Juego: "+getNombre());
+        System.out.println("Categoria: "+getCategoria());
+        System.out.println("Descripción: "+getDescripcion());
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
---
+++ src/Programa/Programa.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Programa;
+
+/**
+ *
+ * @author Andrés Aquin
+ */
+public class Programa {
+
+    protected String EmprDev;
+    protected int nroPlataformas;
+    protected double version;
+    protected double precio;
+
+    public Programa() {
+        this.EmprDev = "Riot Games";
+        this.nroPlataformas = 1;
+        this.version = 11.4;
+        this.precio = 100;
+    }
+
+    public Programa(String emdv, int np, double v, double p) {
+        this.EmprDev = emdv;
+        this.nroPlataformas = np;
+        this.version = v;
+        this.precio = p;
+    }
+    public void leer(){
+        System.out.println("Empresa Desarrolladora: ");
+        setEmprDev(Leer.dato());
+        System.out.println("Número de Plataformas: ");
+        setNroPlataformas(Leer.datoInt());
+        System.out.println("Version: ");
+        setVersion(Leer.datoDouble());
+        System.out.println("Precio: ");
+        setPrecio(Leer.datoDouble());
+    }
+    public void mostrar(){
+        System.out.println("Tech Startup: ");
+        System.out.println(getEmprDev());
+        System.out.println("Plataformas: "+getNroPlataformas());
+        System.out.println("Versión: "+getVersion());
+        System.out.println("Precio: "+getPrecio());
+    }
+
+    public String getEmprDev() {
+        return EmprDev;
+    }
+
+    public void setEmprDev(String emdv) {
+        this.EmprDev = emdv;
+    }
+
+    public int getNroPlataformas() {
+        return nroPlataformas;
+    }
+
+    public void setNroPlataformas(int np) {
+        this.nroPlataformas = np;
+    }
+
+    public double getVersion() {
+        return version;
+    }
+
+    public void setVersion(double v) {
+        this.version = v;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double p) {
+        this.precio = p;
+    }
+}
