b079034d14e848e662c5d998d0d2cb8ea0ea5022
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-20 14:20:38 -0400

    Tema Herencia: Ejercicio Organizacion. la superclase es Organizacion su clase derivada es EntidadEducativa, y sus clases derivadas de Entidad Educativa son Instituto, Normal y Colegio

---
+++ src/Organizacion/EntidadEducacion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Organizacion;
+
+/**
+ *
+ * @author Andrés Aquin
+ */
+public class EntidadEducacion {
+    private String nombre;
+    private int añoFundacion;
+    private String tipo;
+}
---
+++ src/Organizacion/Organizacion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Organizacion;
+
+/**
+ *
+ * @author Andrés Aquin
+ */
+public class Organizacion {
+    protected int NroProfesores;
+    protected int nroEstudiantes;
+    protected String[] Estudiantes = new String[30];
+    protected String[] Profesores = new String[20];
+    
+    
+}
