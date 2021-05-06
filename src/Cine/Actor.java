ad45af33bb436033c159dd681159dfd665723b05
Andrés K. Aquin <andruuus123@gmail.com>
2021-05-06 11:11:14 -0400

    Ejercicio de Agregación y Composicion: Cine con clases: Actor, Cine, Director, Pelicula, Persona, Sala

---
+++ src/Cine/Actor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cine;
+
+/**
+ *
+ * @author Andrés Aquin
+ */
+public class Actor extends Persona{
+    private String personaje;
+    
+    public Actor(String nombre){
+        super(nombre);
+    }
+    
+}
